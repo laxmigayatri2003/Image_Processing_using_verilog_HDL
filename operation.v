a8f9da1c34635f93c1a7ea49e0a0039d4d79530f
laxmigayatri2003 <131864389+laxmigayatri2003@users.noreply.github.com>
2023-07-14 11:24:38 +0530

    Add files via upload

---
+++ operation.v
@@ -0,0 +1,12 @@
+`define INPUTFILENAME		 "input.hex" // Input file name
+`define OUTPUTFILENAME		 "output.bmp"		// Output file name
+
+// Choose the operation of code 
+
+//`define BRIGHTNESS_OPERATION
+//`define INVERT_OPERATION
+//`define BLACKandWHITE_OPERATION
+`define THRESHOLD_OPERATION 
+
+
+
